2d80a18caedc77e930693cd49f613891cf485caa
elessarshaiya <50587074+elessarshaiya@users.noreply.github.com>
2024-11-29 22:37:49 +0300

    Create disable some logs.sql

---
+++ SQL/disable some logs.sql
@@ -0,0 +1,11 @@
+-- disable gm commands,login,logout
+-- usp_Insert_Action_Log_E
+SET @Sql = N'
+IF (@ActionType NOT IN (180, 108, 107))
+BEGIN
+INSERT INTO PS_GameLog.dbo.ActionLog
+(UserID, UserUID, CharID, CharName, CharLevel, CharExp, MapID,  PosX, PosY, PosZ, ActionTime, ActionType, 
+Value1, Value2, Value3, Value4, Value5, Value6, Value7, Value8, Value9, Value10, Text1, Text2, Text3, Text4)
+VALUES(@UserID, @UserUID, @CharID, @CharName, @CharLevel, @CharExp, @MapID, @PosX, @PosY, @PosZ, @ActionTime, @ActionType, 
+@Value1, @Value2, @Value3, @Value4, @Value5, @Value6, @Value7, @Value8, @Value9, @Value10, @Text1, @Text2, @Text3, @Text4)
+END'
